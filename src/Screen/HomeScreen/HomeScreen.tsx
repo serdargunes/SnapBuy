d75d3bf7b3a16674bd67b36555246918169a9188
Serdargunes <95936184+serdargunes@users.noreply.github.com>
2025-02-17 00:40:54 +0300

    HomeScreen edit

--- src/Screen/HomeScreen/HomeScreen.tsx
+++ src/Screen/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {ScrollView,Text } from 'react-native'
 import Header from '../../components/Header/Header'
+import Banner from '../../components/Banner/Banner'
+import ProductsDetail from '../../components/ProductsDetail/ProductsDetail'
 
 function HomeScreen() {
   return (
     <ScrollView>
       <Header />
+      <Banner />
+      <ProductsDetail />
       
     </ScrollView>
   )
