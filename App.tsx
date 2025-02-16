d542c56d338608b342d96e0372e7218d46e07005
Serdargunes <95936184+serdargunes@users.noreply.github.com>
2025-02-16 21:24:53 +0300

    HomeScreen Page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/Screen/HomeScreen/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <HomeScreen />
+    </ScrollView>
   );
 }
 
