fda324dd1ed570f002656a0db798a58ced915716
Serdargunes <95936184+serdargunes@users.noreply.github.com>
2025-02-17 18:22:14 +0300

    HomeScreen improved

--- src/Screen/HomeScreen/HomeScreen.tsx
+++ src/Screen/HomeScreen/HomeScreen.tsx
@@ -3,13 +3,14 @@ import {ScrollView,Text } from 'react-native'
 import Header from '../../components/Header/Header'
 import Banner from '../../components/Banner/Banner'
 import ProductsDetail from '../../components/ProductsDetail/ProductsDetail'
+import Products from '../../components/Products/Products'
 
 function HomeScreen() {
   return (
     <ScrollView>
       <Header />
       <Banner />
-      <ProductsDetail />
+      <Products />
       
     </ScrollView>
   )
