4c84760afa925dd60eb427085db47d5d33625856
Serdargunes <95936184+serdargunes@users.noreply.github.com>
2025-02-18 17:06:25 +0300

    Products page has been made

--- src/Screen/ProductsScreen/ProductsScreen.tsx
+++ src/Screen/ProductsScreen/ProductsScreen.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import {StyleSheet, ScrollView, Text,Image,View } from 'react-native'
+import {StyleSheet, ScrollView, Text,Image,View, Button, TouchableOpacity } from 'react-native'
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 function ProductsScreen() {
   return (
     <ScrollView>
         <View>
-            <Image style = {styles.image} source={require('../../../assets/candle.jpeg')}/>
+            <Image style = {styles.image} source={require('../../../assets/toydeer.jpeg')}/>
         </View>
         <View style = {styles.titlebox}>
         <Text style = {styles.title}>New Year's toy deer</Text>
         <Text style = {styles.star}><AntDesign name="star" size={24} color="orange" /></Text>
         <Text style = {styles.puan}>4.9</Text>
+        </View>
+        <View>
+            <Text style = {styles.explanation} numberOfLines={5} ellipsizeMode='tail'>Celebrate the New Year with this beautifully crafted wooden toy deer! Made from high-quality wood, this charming decoration adds a touch of rustic elegance to your festive décor. Its smooth finish and intricate detailing make it a perfect centerpiece or a delightful gift for loved ones</Text>
+        </View>
+        <View style = {styles.addtocartContainer}>
+            <Text style={styles.addtocartPrice} >$12.99</Text>
+            <TouchableOpacity style = {styles.button}>
+            <Text style = {styles.addtocartText}>Add to cart</Text>
+            </TouchableOpacity>
         </View>
 
     </ScrollView>
@@ -19,14 +28,16 @@ function ProductsScreen() {
 }
 const styles = StyleSheet.create({
     image:{
-        height:450,
+        width:500,
+        height:500,
+        
     },
     titlebox:{
         marginTop:20,
-        marginLeft:20,
+        marginLeft:-5,
         flexDirection:'row',
         justifyContent:'center',
-        alignItems:'center',
+        
         
     },
 
@@ -43,6 +54,45 @@ const styles = StyleSheet.create({
         fontSize:25,
         fontWeight:'500',
     },
+    explanation:{
+        fontSize:17,
+        fontWeight:'500',
+        marginTop:10,
+        marginLeft:20,
+        justifyContent:'center',
+        width:400,
+    },
+    addtocartContainer:{
+        backgroundColor:'white',
+        marginTop:40,
+        flexDirection:'row',
+        height:130,
+        justifyContent:'center',
+        alignItems:'center',
 
+    },
+    addtocartPrice:{
+        fontSize:30,
+        fontWeight:'500',
+        marginLeft:40,
+        
+    },
+    button:{
+        fontSize:30,
+        backgroundColor:'black',
+        marginLeft:65,
+        borderRadius:40,
+        height:70,
+        width:280,
+        justifyContent:'center',
+        alignItems:'center',
+        marginRight:30,
+        
+    },
+    addtocartText:{
+        fontSize:30,
+        color:'white',
+
+    },
 })
 export default ProductsScreen
