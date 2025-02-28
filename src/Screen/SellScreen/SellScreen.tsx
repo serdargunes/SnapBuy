900d1031510ae7685a3ac11ad053ef79a97d4fde
Serdargunes <95936184+serdargunes@users.noreply.github.com>
2025-02-28 18:20:44 +0300

    The sales page in the mobile application is finished

--- src/Screen/SellScreen/SellScreen.tsx
+++ src/Screen/SellScreen/SellScreen.tsx
@@ -2,46 +2,65 @@ import React from "react";
 import { View, Text, Image, FlatList, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import { useProduct } from "../../context/ProductContext/ProductContext";
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 function SellPage() {
-    const { products } = useProduct(); // Context'ten ürünleri al
+    const { products } = useProduct();
 
 
     return (
-        <ScrollView>
-        <View style={styles.container}>
+        <ScrollView style={{ backgroundColor: 'white' }}>
+            <View style={styles.container}>
 
-            <FlatList
-                data={products}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <View style={styles.product}>
-                        <Image style={styles.image} source={{ uri: item.image }} />
-                        <View style={styles.textbox}>
-                            <Text style={{ fontSize: 18, fontWeight: '500', marginTop: 10, }}>{item.title}</Text>
-                            <Text style={{ fontSize: 18, opacity: 0.5, marginTop: 10, }}>White</Text>
-                            <Text style={{ fontSize: 18, fontWeight: 'bold', marginTop: 25 }}>{item.price}</Text>
-                            <TouchableOpacity style={styles.exit}>
-                                <Text><FontAwesome6 name="x" size={18} color="gray" /></Text>
-                            </TouchableOpacity>
-                            <View style={{ flexDirection: 'row', marginTop: -30, marginLeft: 130, alignItems: 'center', justifyContent: 'center' }}>
-                                <TouchableOpacity>
-                                    <Text style={styles.minus}>-</Text>
+                <FlatList
+                    data={products}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <View style={styles.product}>
+                            <Image style={styles.image} source={{ uri: item.image }} />
+                            <View style={styles.textbox}>
+                                <Text style={{ fontSize: 18, fontWeight: '500', marginTop: 10, }}>{item.title}</Text>
+                                <Text style={{ fontSize: 18, opacity: 0.5, marginTop: 10, }}>White</Text>
+                                <Text style={{ fontSize: 18, fontWeight: 'bold', marginTop: 25 }}>{item.price}</Text>
+                                <TouchableOpacity style={styles.exit}>
+                                    <Text><FontAwesome6 name="x" size={18} color="gray" /></Text>
                                 </TouchableOpacity>
-                                <Text style={{ fontSize: 20, fontWeight: '400' }}>1</Text>
-                                <TouchableOpacity>
-                                    <Text style={styles.plus}>+</Text>
-                                </TouchableOpacity>
+                                <View style={{ flexDirection: 'row', marginTop: -30, marginLeft: 130, alignItems: 'center', justifyContent: 'center' }}>
+                                    <TouchableOpacity>
+                                        <Text style={styles.minus}>-</Text>
+                                    </TouchableOpacity>
+                                    <Text style={{ fontSize: 20, fontWeight: '400' }}>1</Text>
+                                    <TouchableOpacity>
+                                        <Text style={styles.plus}>+</Text>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
+
                         </View>
-                        
-                    </View>
-                )}
-            />
-        </View>
-        <View>
-            <Text>New2025</Text>
-        </View>
+                    )}
+                />
+            </View>
+            <View style={styles.seasonbox}>
+                <Text style={styles.season}>New2025</Text>
+                <Text style={styles.applied}>Promocode appiled <AntDesign name="checkcircle" size={24} color="green" /></Text>
+            </View>
+            <View style={styles.paycontainer}>
+                <View style={styles.pay1}>
+                    <Text style={styles.textpay}>Subtotal: </Text>
+                    <Text style={styles.textpay}>Delivery Fee: </Text>
+                    <Text style={styles.textpay}>Discount: </Text>
+                </View>
+                <View style={styles.pay2}>
+                    <Text style={styles.text$}>$34.99</Text>
+                    <Text style={styles.text$}>$5.00</Text>
+                    <Text style={styles.text$}>20%</Text>
+                </View>
+            </View>
+            <View style={styles.shadow}>
+                <TouchableOpacity style={styles.checkbox}>
+                    <Text style={styles.checktext}>Check out for $31.99</Text>
+                </TouchableOpacity>
+            </View>
         </ScrollView>
     );
 }
@@ -63,7 +82,11 @@ const styles = StyleSheet.create({
     {
         flexDirection: "row",
         alignItems: "center",
-        marginBottom: 10
+        marginBottom: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: "lightgray",
+        width: "100%",
+        paddingBottom:15,
     },
     image:
     {
@@ -104,6 +127,98 @@ const styles = StyleSheet.create({
         marginRight: 10,
         fontWeight: '300',
     },
+    seasonbox: {
+        marginTop: -15,
+        height: 70,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderWidth: 3,
+        borderColor: 'gray',
+        alignItems: 'center',
+        marginLeft: 20,
+        marginRight: 20,
+
+
+    },
+    season: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        marginLeft: 20,
+
+    },
+    applied: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'green',
+        marginRight: 20,
+
+    },
+    paycontainer: {
+        flexDirection: "row",
+        width: "100%",
+        paddingHorizontal: 20,
+        marginTop:-10,
+    },
+    pay1: {
+        width: 200,
+        flex: 1,
+        padding: 20,
+        alignItems: "flex-start",
+    },
+    pay2: {
+        width: 200,
+        flex: 1,
+        padding: 20,
+        alignItems: "flex-end"
+    },
+
+
+    textpay: {
+        marginTop: 20,
+        fontSize: 16,
+        fontWeight: '300',
+
+    },
+    text$: {
+        marginTop: 20,
+        fontSize: 16,
+        fontWeight: '500',
+
+    },
+    shadow: {
+        height: 200,
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 0,
+    },
+    checkbox: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 70,
+        width: 410,
+        borderRadius: 100,
+        borderWidth: 1,
+        marginLeft: 0,
+        marginTop: -60,
+        backgroundColor: 'black',
+
+
+
+    },
+    checktext: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: '500',
+        textAlign: 'center',
+
+    },
+
 });
 
 export default SellPage;
